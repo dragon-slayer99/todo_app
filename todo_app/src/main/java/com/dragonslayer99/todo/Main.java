82b607f215587c5d106e3e61d1e3d8e044dcf122
dragon-slayer99 <guudurumahesh@gmail.com>
2026-01-06 18:28:22 +0530

    Refactor command handling in Main class and update todos.json format
    - Improved command processing in Main class by replacing if-else statements with a switch-case structure for better readability and maintainability.
    - Reformatted todos.json to enhance readability by adding indentation and line breaks for each todo item.
    - Ensured consistent formatting in todos.json to facilitate easier updates and debugging.

--- todo_app/src/main/java/com/dragonslayer99/todo/Main.java
+++ todo_app/src/main/java/com/dragonslayer99/todo/Main.java
@@ -26,25 +26,31 @@ public class Main {
                 try (Scanner scanner = new Scanner(System.in)) {
                         String usercommand;
 
-                        while (true) {
-
-                                System.out.println("Enter command: ");
-                                usercommand = scanner.nextLine();
-
-                                if (usercommand.contains("add")) {
-                                        command.addTodo(usercommand);
-                                } else if (usercommand.contains("update")) {
-                                        command.updateTodo(usercommand);
-                                } else if (usercommand.contains("delete")) {
-                                        command.deleteTodo(usercommand);
-                                } else if (usercommand.contains("display")) {
-                                        command.display();
-                                } else if (usercommand.contains("exit")) {
-                                        break;
-                                } else {
-                                        System.out.println("Please enter valid command!");
-                                }
+                    OUTER:
+                    while (true) {
+                        System.out.println("Enter command: ");
+                        usercommand = scanner.nextLine();
+                        String scannerCmd[] = usercommand.split(" ");
+                        switch (scannerCmd[0]) {
+                            case "add":
+                                command.addTodo(usercommand);
+                                break;
+                            case "update":
+                                command.updateTodo(usercommand);
+                                break;
+                            case "delete":
+                                command.deleteTodo(usercommand);
+                                break;
+                            case "display":
+                                command.display();
+                                break;
+                            case "exit":
+                                break OUTER;
+                            default:
+                                System.out.println("Please enter valid command!");
+                                break;
                         }
+                    }
                 } catch (Exception e) {
                         System.out.println(DisplayInstructions.RED + "Erro while reading user input"
                                         + DisplayInstructions.RESET);
